9d83df502bcdb159b392fe30ccd74363058700b7
Bryan Loaiza Rodríguez <bryan-loa95@hotmail.com>
2020-10-15 10:27:23 -0500

    Se desarrolla toda la lógica de las listas. Se permite desplazar las tareas de un lado a otro y contar el número de tarea realizadas o pendientes

--- src/js/main.js
+++ src/js/main.js
@@ -27,8 +27,17 @@ let list = [
 ];
 
 const listContainer = document.querySelector("#list-content");
+const listToDo=document.querySelector("#todo-list");
+const nolistToDo=document.querySelector("#noTodo-list");
+const titleNoToDo=document.querySelector("#TitlenoTodoList");
+const titleToDo=document.querySelector("#TitleTodoList");
+
+let itemsDo=0;
+let itemsNoDo=0;
+
 
 paintList(list);
+paintTasksList(list);
 
 function createTask(task) {
   const input = document.querySelector("#inputTask");
@@ -43,10 +52,15 @@ function createTask(task) {
 
 const checkTask = (checkbox, id) => {
   const task = list.find((element) => {
+
     return element.id === id;
   });
+
   task.done = checkbox.checked;
+
+ 
   paintList(list);
+  paintTasksList(list);//Function that implements the item's movement
 };
 
 function paintList(lst) {
@@ -64,3 +78,56 @@ function renderListItem(item) {
               <input type="checkbox" ${checked} aria-label="Checkbox for following text input" onclick="checkTask(this, ${item.id})"> ${item.name}
           </li>`;
 }
+//----------------------------------------Bryan LOGIC ---------------------------------------------------------------//
+
+function paintTasksList(e){
+  let elementsList="";
+  let elementNoList="";
+ 
+  e.forEach((element)=>{
+    if(!element.done){
+      elementsList+=RenderItem(element);
+    }
+
+    if(element.done){
+      elementNoList+=RenderItem(element);
+    }
+  });
+  let numberDo=NumberToDoList();
+  let numberNoDo=NumberNoToDoList();
+
+  listToDo.innerHTML=elementsList;
+  titleToDo.innerHTML=numberDo;
+  nolistToDo.innerHTML=elementNoList;
+  titleNoToDo.innerHTML=numberNoDo;
+ 
+}
+
+function RenderItem(TodoItem){
+  
+  return `<li class="list-group-item list item">
+              <p> ${TodoItem.name}</p>
+          </li>`
+}
+
+function NumberToDoList(){
+  let number=0;
+  list.forEach((element)=>{
+    if(!element.done){
+      number=number+1;
+    }
+  });
+  return `<h5 class="card-title" id="TitleTodoList">Todo List <span class="numberStyle">${number}</span></h5>`
+};
+
+function NumberNoToDoList(){
+  let number=0;
+  list.forEach((element)=>{
+    if(element.done){
+      number=number+1;
+    }
+  });
+  return `<h5 class="card-title" id="TitlenoTodoList">No Todo List<span class="numberStyle">${number}</span></h5>`
+};
+
+
